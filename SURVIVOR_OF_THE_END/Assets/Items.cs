f1e78ace0ec0138f0d1e282d74e3e68dc436fcc6
imannajihahzainol <iman060415@gmail.com>
2025-11-26 01:20:13 +0800

    gun script

--- SURVIVOR_OF_THE_END/Assets/Items.cs
+++ SURVIVOR_OF_THE_END/Assets/Items.cs
@@ -68,37 +68,4 @@ namespace Assembly_CSharp
     }
 
 
-    public class Gun : Weapons
-    {
-        public int bullets = 6;
-        public int gunDamage = 50;
-        public ParticleSystem muzzleFlash;
-        public AudioSource shootSound;
-
-        protected void Awake()
-        {
-            Initialize("Gun");
-        }
-
-        public void Shoot(Zombie target)
-        {
-            if (bullets <= 0) return;
-
-            bullets--;
-
-            if (target != null && !target.IsDead())
-                target.TakeDamage(gunDamage);
-
-            if (muzzleFlash != null) muzzleFlash.Play();
-            if (shootSound != null) shootSound.Play();
-
-            Animator animator = GetComponent<Animator>();
-            if (animator != null) animator.SetTrigger("Shoot");
-        }
-
-        public override void Attack(Zombie target)
-        {
-            Shoot(target);
-        }
-    }
 }
